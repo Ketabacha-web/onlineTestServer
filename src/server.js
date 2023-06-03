56c47b5d7c4fee17bc1460558c500bead8ef435c
mudaser-sayeedi <mudasersayeed@gmail.com>
2023-06-03 13:34:35 +0430

    now insert admin user when server new started into the database if not exist!

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import { createServer } from "http";
 import { app } from "./express.js";
 import * as dotenv from "dotenv";
 import { Log } from "./components/logger.component.js";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
+import { db } from "./config/mySql.db.connection.config.js";
 dotenv.config();
 
 // CREATING NODEJS SERVER
@@ -10,12 +13,48 @@ const server = createServer(app);
 // -----------------------
 
 // FETCHING PORT VALUE FROM ENVIRONMENT VARIABLES .env FILE IN THE SERVER
-const PORT = process.env.PORT||10000;
+const PORT = process.env.PORT || 10000;
 // -----------------------
 
 // STARTING SERVER IN SPECIFIC PORT
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
   Log.info("server is lived on the network !!!");
   Log.info(`server is running on port: ${PORT}`);
+
+  // INSERT ADMIN USER IF NOT EXIST TO THE DATABASE!!!
+  try {
+    // Check if the admin user exists in the database
+    const adminUser = await db("users")
+      .where({ email: "test.mail.developer.jsx@gmail.com" })
+      .first();
+
+    // If the admin user doesn't exist, insert it into the database
+    if (!adminUser) {
+      // Hash and salt the password
+      const hashedPassword = await bcrypt.hash("Test@1234", 10);
+
+      const verificationToken = crypto.randomBytes(20).toString("hex");
+
+      await db("users").insert({
+        name: "admin",
+        lastname: "admin",
+        email: "test.mail.developer.jsx@gmail.com",
+        password: hashedPassword,
+        role: "admin",
+        verification_token: verificationToken,
+        is_verified: 1,
+        is_actived: 1,
+        // Add other admin user details as needed
+      });
+      Log.info("Admin user inserted into the database.");
+    } else {
+      Log.info("Admin user already exists in the database.");
+    }
+
+    // Close the database connection
+    await db.destroy();
+  } catch (error) {
+    Log.error("Error occurred while inserting admin user:", error);
+  }
 });
 // -----------------------
